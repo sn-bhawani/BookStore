c3dcdfa12eb3282a4487ced332bc145781438926
sn-bhawani <odishadeveloper16@gmail.com>
2026-07-25 15:55:36 +0530

    about merge to main

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ app.controller("storemanager", function($scope) {
         },
         {
             id: 7,
-<<<<<<< HEAD
             name: "The Mental Models",
             author: "Peter Hollins",
             price: 300,
@@ -90,10 +89,8 @@ app.controller("storemanager", function($scope) {
             id: 6,
             name: "The Pragmatic Programmer",
             author: "Andrew Hunt",
-=======
             name: "Good Vibe,Good Life",
             author: "Vex King",
->>>>>>> about-conatct
             price: 1000,
             url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR_hH8We57dd286hnqQFHJY2l2K6kqHosdCGk_IxM4kLFmSNfrSEAfLY-IMeGkmkgqRbMPvFgQtvFmDIuQjGbsm79pZPMzgFM8GxAPqH0VSKahtGBgpVP0k"
         }
