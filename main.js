26bbfcae39c86cc6328c400e3ef7e2d6b0f26f21
sn-bhawani <odishadeveloper16@gmail.com>
2026-07-25 13:01:13 +0530

    Initial commit of BookStore HTML project

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = angular.module("bookStore", []);
+app.controller("storemanager", function($scope) {
+
+    $book_store = [
+        {
+            bookId: 1,
+            bookName: "The Mental Models",
+            authorName: "Peter Hollins",
+        }
+    ]
+
+});
